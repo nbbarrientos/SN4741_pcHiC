89d9b0019970f5b7334c3bde3a143d3c65b575c1
Nelson Barrientos <62807501+nbbarrientos@users.noreply.github.com>
2022-12-13 14:38:56 -0500

    Update hicup.sh

--- HiCUP/hicup.sh
+++ HiCUP/hicup.sh
@@ -4,7 +4,7 @@ module load bowtie2/2.4.1
 module load r/4.0.2
 module load samtools/1.10
 
-sbatch -A amccall2 \
+sbatch -A <allocation> \
         -p defq \
         -N 1 \
         -n 24 \
@@ -12,6 +12,6 @@ sbatch -A amccall2 \
         -t 72:00:00 \
         -J "Running HiCUP combined" \
         --mail-type=END \
-        --mail-user=nbarrie1@jhu.edu \
+        --mail-user=<email> \
         --wrap="hicup --config hicup_config_combined.conf"
 
